8f7de8f5eecd6f08af78a3ff9c58dba00dfed8be
Marko Banusic <mbanusic@gmail.com>
2017-05-02 11:46:13 +0200

    Add zoom

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,19 +6,25 @@ var width = 500,
 
 var svg = d3.select("#map").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .call(d3.zoom().on("zoom", function () {
+        svg.attr("transform", d3.event.transform)
+    }))
+    .append("g");
+
+var projection = d3.geoMercator()
+    .center([16.4, 44.7])
+    .scale(4500)
+    .translate([width / 2, height / 2]);
+var path = d3.geoPath()
+    .projection(projection);
 
 d3.json("js/hrv.json", function(error, uk) {
     if (error) return console.error(error);
-    var projection = d3.geoMercator()
-        .center([16.4, 44.7])
-        .scale(4500)
-        .translate([width / 2, height / 2]);
-    var path = d3.geoPath()
-        .projection(projection);
 
     svg.append("path")
         .datum(topojson.feature(uk, uk.objects.subunits))
         .attr("class", "hrvatska")
-        .attr("d", path);
+        .attr("d", path)
+        .on('click', function(d) {console.log(path.bounds(d))});
 });
