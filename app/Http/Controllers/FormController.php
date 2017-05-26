5b882123095394f4b29a668d1d6467a3a8c0c924
Marko Banusic <mbanusic@gmail.com>
2017-05-26 21:41:27 +0200

    Add more cors

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -9,6 +9,7 @@ class FormController extends Controller {
 
 	public function __construct() {
 		$this->middleware('auth')->except(['postForm', 'showPublic', 'getEntries']);
+		$this->middleware('cors')->only(['postForm', 'getEntries']);
 	}
 
 	public function showPublic() {
