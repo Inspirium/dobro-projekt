251b135bc2b823acda8e88f64f9c85903c980043
Marko Banusic <mbanusic@gmail.com>
2017-05-30 10:54:13 +0200

    Show latest

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -101,7 +101,7 @@ function dbl(d, that) {
 }
 
 function create_dataset(lands) {
-    var data = [], out = [], radius = 10;
+    var data = [], out = [], radius = 8;
     if (window.innerWidth < 600) {
         radius = radius/2;
     }
